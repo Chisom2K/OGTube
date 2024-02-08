d428c0a9dec41d5d1b7190a41a68d0ee1e645823
chisom <chisombiriogbuefi@gmail.com>
2024-02-07 21:51:49 -0600

    update

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,34 +1,55 @@
 import express from 'express';
-import ffmpeg from 'fluent-ffmpeg';
+import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from './storage';
+
+setupDirectories();
 
 const app = express();
 app.use(express.json());
 
-app.post('/process-video', (req, res) => {
+app.post('/process-video', async (req, res) => {
+  // Get the bucket and filename from the cloud Pub/Sub message
+  let data;
+  try{
+    const message = Buffer.from(req.body.message.data, 'base64').toString('utf8'); // takes message from cloud Pub/Sub
+    data = JSON.parse(message); // Parses the message 
+    if(!data.name){ // checks if name actually exists 
+      throw new Error('Invalid message payload received');
+    }
+  }catch(error){
+    console.error(error);
+    return res.status(400).send('Bad Request:missing filename.'); // returns error
+  }
+  const inputFileName = data.name;
+  const outputFileName = `processed-${inputFileName}`;
 
-  // Get the path of the input video file from the request body
-  const inputFilePath = req.body.inputFilePath;
-  const outputFilePath = req.body.outputFilePath;
+  //Downloading raw video from cloud storage
+  await downloadRawVideo(inputFileName);
 
-  // Check if the input file path is defined
-  if (!inputFilePath || !outputFilePath) {
-    res.status(400).send('Bad Request: Missing file path');
+  // Convert video to 720p
+  try{
+    convertVideo(inputFileName, outputFileName)
+  }catch(err) {
+    await Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(outputFileName)
+    ]);
+    console.error(err);
+    return res.status(500).send('Internal Server Error: Processing failed.');
   }
 
-  // Create the ffmpeg command
-  ffmpeg(inputFilePath)
-    .outputOptions('-vf', 'scale=-2:720') // 720p
-    .on('end', function() {
-        res.status(200).send('Processing finished successfully');
-    })
-    .on('error', function(err: any) {
-        console.log(`An error occurred: ${err.message}`);
-        res.status(500).send(`An error occurred: ${err.message}`);
-    })
-    .save(outputFilePath);
+  //Upload the processed video to Cloud Storage 
+  await uploadProcessedVideo(outputFileName)
+
+  await Promise.all([
+    deleteRawVideo(inputFileName),
+    deleteProcessedVideo(outputFileName)
+  ]);
+
+  return res.status(200).send('Process Finished Successfully');
+  
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
